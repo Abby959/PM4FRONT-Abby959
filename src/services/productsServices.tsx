3a177b3b9d41bcbcadd89930603efdaefab02715
Abby <abygarcia3243@gmail.com>
2024-09-29 22:54:02 -0300

    change url place

--- src/services/productsServices.tsx
+++ src/services/productsServices.tsx
@@ -1,14 +1,14 @@
 import { Products } from "@/app/interface";
 
-const url = `${process.env.API_URL}/products`;
-
 export const getProductsService = async () => {
+    const url = `${process.env.API_URL}/products`;
     const response = await fetch(url, { next: { revalidate: 0 } });
     return await response.json() as Products[];
 }
 
 export const getProductsByIdService = async (id: number) => {
-    const response = await fetch(url, {next: {revalidate: 0}});
+    const url = `${process.env.API_URL}/products`;
+    const response = await fetch(url, { next: { revalidate: 0 } });
     const products = await response.json() as Products[];
-    return products.filter( (p) => p.id === Number(id))[0]
+    return products.filter((p) => p.id === Number(id))[0]
 }
